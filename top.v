ef060abceff3bbe91fe66b33c0a2fb56e418b36e
TRISTINWATKINS <156130938+TRISTINWATKINS@users.noreply.github.com>
2025-02-20 14:28:49 -0700

    Add files via upload

---
+++ top.v
@@ -0,0 +1,18 @@
+module top(
+    input [1:0]sw,
+    output [1:0]led
+);
+    half_sub half_sub1(
+        .A(sw[0]),
+        .B(sw[1]),
+        .Y(led[0]),
+        .Borrow(led[1])
+    );
+    
+    ones_compliment ones_compliment1(
+        .A(sw[5:2]),
+        .B(sw[9:6]),
+        .Y(led[5:2])
+    );
+    
+endmodule
